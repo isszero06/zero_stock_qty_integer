5accd5f01035f0114b399f96eb3c74157b56dadb
Zero Systems <isszero06@gmail.com>
2024-11-24 05:12:08 +0200

    Update __init__.py

--- zero_stock_qty_integer/__init__.py
+++ zero_stock_qty_integer/__init__.py
@@ -17,5 +17,5 @@ def pre_init_check(cr):
     from odoo.exceptions import UserError
     version_info = common.exp_version()
     server_serie = version_info.get('server_serie')
-    if server_serie!='17.0':raise UserError('Module support Odoo series 17.0 found {}.'.format(server_serie))
+    if server_serie!='18.0':raise UserError('Module support Odoo series 18.0 found {}.'.format(server_serie))
     return True
