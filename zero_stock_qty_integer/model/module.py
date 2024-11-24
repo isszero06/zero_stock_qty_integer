0a79a3b467a681b194d42235c9c25891545881ff
Zero Systems <isszero06@gmail.com>
2024-11-24 10:23:33 +0200

    Update module.py

--- zero_stock_qty_integer/model/module.py
+++ zero_stock_qty_integer/model/module.py
@@ -19,6 +19,12 @@ from odoo.tools import float_is_zero, float_round, float_compare, OrderedSet
 import logging
 _logger = logging.getLogger(__name__)
 
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+
+    is_int = fields.Boolean(string="Stock move must integer number?",default=True)
+
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
@@ -26,6 +32,6 @@ class StockMove(models.Model):
         res = super(StockMove, self)._action_done(cancel_backorder=cancel_backorder)
         for move in self:
             quantity_int = (int(float_round(move.quantity, precision_digits=0,precision_rounding=1, rounding_method='DOWN')))       
-            if move.quantity != quantity_int:
+            if move.product_id.is_int and move.quantity != quantity_int:
                 raise UserError(_('You cannot perform the move of Quantity Not Integer!.(Product: %s)', move.name))
         return res            
