2aef111b5e5856ae33ec7a1d1eb310fad5b38982
Zero Systems <isszero06@gmail.com>
2025-03-25 01:34:18 +0200

    Update module.py

--- zero_stock_qty_integer/model/module.py
+++ zero_stock_qty_integer/model/module.py
@@ -34,4 +34,16 @@ class StockMove(models.Model):
             quantity_int = (int(float_round(move.quantity, precision_digits=0,precision_rounding=1, rounding_method='DOWN')))       
             if move.product_id.is_int and move.quantity != quantity_int:
                 raise UserError(_('You cannot perform the move of Quantity Not Integer!.(Product: %s)', move.name))
+        for move_line in self.move_line_ids:
+            move_line._qty_integer_product()
         return res            
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    @api.onchange('quantity','lot_id')
+    def _qty_integer_product(self):
+        for move_line in self:
+            quantity_int = (int(float_round(move_line.quantity, precision_digits=0,precision_rounding=1, rounding_method='DOWN')))       
+            if move_line.product_id.is_int and move_line.quantity != quantity_int:
+                raise UserError(_('You cannot perform the move of Quantity Not Integer!.(Product: %s)', move_line.product_id.name))
